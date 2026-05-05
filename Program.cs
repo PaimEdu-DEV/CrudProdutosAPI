b3b08ba2d3a3879a1fccc10d5c11650fbd88c9ee
PaimEdu-DEV <epaim787@gmail.com>
2026-05-05 15:57:50 -0300

    adicionado front end e integracao com sql server

--- Program.cs
+++ Program.cs
@@ -2,30 +2,27 @@ using Microsoft.EntityFrameworkCore;
 using APIExercicio.Data;
 using System.Globalization;
 
+
 var builder = WebApplication.CreateBuilder(args);
-
-// 👇 AGORA sim pode usar o builder
 string SortVersion = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(SortVersion));
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+    // ✅ CORS adicionado — permite que o front-end acesse a API
+    builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
+app.UseCors(); // ✅ Ativa o middleware CORS
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
